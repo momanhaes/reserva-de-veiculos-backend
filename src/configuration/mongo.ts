3eac02c1516b134982eab69f1eec4e8b441d5bee
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-14 17:03:26 -0300

    feat: Insere variáveis de ambiente pro heroku

--- src/configuration/mongo.ts
+++ src/configuration/mongo.ts
@@ -1,27 +1,27 @@
-import { Mongoose, connect, disconnect } from 'mongoose';
-import { IConnectionProvider } from '../typings';
+import { Mongoose, connect, disconnect } from "mongoose";
+import { IConnectionProvider } from "../typings";
 
 export default class implements IConnectionProvider {
-    private static _instance: IConnectionProvider;
-    private connection: Mongoose;
+  private static _instance: IConnectionProvider;
+  private connection: Mongoose;
 
-    static getInstance(): IConnectionProvider {
-        if (!this._instance) this._instance = new this;
+  static getInstance(): IConnectionProvider {
+    if (!this._instance) this._instance = new this();
 
-        return this._instance;
+    return this._instance;
+  }
+
+  async connect(): Promise<void | never> {
+    if (!this.connection) {
+      this.connection = await connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      });
     }
+  }
 
-    async connect(): Promise<void | never> {
-        if (!this.connection) {
-            this.connection = await connect('mongodb://localhost:27017/nodeapi', {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true
-            });
-        }
-    }
-
-    async disconnect(): Promise<void | never> {
-        await disconnect();
-    }
+  async disconnect(): Promise<void | never> {
+    await disconnect();
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import * as http from "http";
 import { app } from "./app";
 
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(3000, () => {
-  console.log(`🚀 Server ready at http://localhost:3000`);
+server.listen(port, () => {
+  console.log(`🚀 Server ready on port ${port}`);
 });
 
 export default server;
